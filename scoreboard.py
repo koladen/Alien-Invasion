c55ca1cdd51a47e6387a32ec7b2f6f260780a322
koladen <koladen@mail.ru>
2021-01-18 13:37:52 +0400

    Рефакторинг scoreboard.py Можно добавить фабрику для отображения уровня, счета и максимального счета

--- scoreboard.py
+++ scoreboard.py
@@ -2,6 +2,7 @@ import pygame.font
 from pygame.sprite import Group
 from ship import Ship
 
+
 class Scoreboard():
     """Класс для вывода игровой информации."""
     def __init__(self, ai_settings, screen, stats):
@@ -21,17 +22,44 @@ class Scoreboard():
 
     def prep_score(self):
         """Преобразует текущий счет в графическое изображение."""
+        target = self.stats.score
+        text = "Счет: "
+        self.prep(target, text)
 
-        rounded_score = round(self.stats.score, -1)
-        score_str = "Счет: {:,}".format(rounded_score)
+    def prep_high_score(self):
+        """Преобразует рекордный счет в графическое изображение."""
+        target = self.stats.high_score
+        text = "Рекорд: "
+        self.prep(target, text)
 
-        self.score_image = self.font.render(score_str, True, self.
-                                                 text_color,
-                                                 self.ai_settings.bg_color)
-        # Вывод счета в правой верхней части экрана.
-        self.score_rect = self.score_image.get_rect()
-        self.score_rect.right = self.screen_rect.right - 20
-        self.score_rect.top = 20
+    def prep_level(self):
+        """Преобразует уровень в графическое изображение."""
+        target = self.stats.level
+        text = f"Уровень: {str(target)}"
+        self.prep(target, text)
+
+    def prep(self, target, text):
+        rounded_score = round(target, -1)
+        score_str = text + f"{rounded_score}"
+        if target == self.stats.score:
+            self.score_image = self.create_score_image(score_str)
+            self.score_rect = self.score_image.get_rect()
+            self.score_rect.right = self.screen_rect.right - 20
+            self.score_rect.top = 20
+        elif target == self.stats.high_score:
+            self.high_score_image = self.create_score_image(score_str)
+            self.high_score_rect = self.high_score_image.get_rect()
+            self.high_score_rect.centerx = self.screen_rect.centerx
+            self.high_score_rect.top = self.score_rect.top
+        elif target == self.stats.level:
+            self.level_image = self.create_score_image(text)
+            self.level_rect = self.level_image.get_rect()
+            self.level_rect.right = self.score_rect.right
+            self.level_rect.top = self.score_rect.bottom + 10
+
+    def create_score_image(self, score_str):
+        return self.font.render(score_str, True, self.text_color, self.ai_settings.bg_color)
+
 
     def show_score(self):
         """Выводит счет на экран."""
@@ -39,27 +67,6 @@ class Scoreboard():
         self.screen.blit(self.high_score_image, self.high_score_rect)
         self.screen.blit(self.level_image, self.level_rect)
         self.ships.draw(self.screen)
-
-    def prep_high_score(self):
-        """Преобразует рекордный счет в графическое изображение."""
-        high_score = int(round(self.stats.high_score, -1))
-        high_score_str = "Рекорд: {:,}".format(high_score)
-        self.high_score_image = self.font.render(high_score_str, True,
-                                                 self.text_color, self.ai_settings.bg_color)
-        # Рекорд выравнивается по центру верхней стороны.
-        self.high_score_rect = self.high_score_image.get_rect()
-        self.high_score_rect.centerx = self.screen_rect.centerx
-        self.high_score_rect.top = self.score_rect.top
-
-    def prep_level(self):
-        """Преобразует уровень в графическое изображение."""
-
-        self.level_image = self.font.render('Уровень: ' + str(self.stats.level), True,
-                                            self.text_color, self.ai_settings.bg_color)
-        # Уровень выводится под текущим счетом.
-        self.level_rect = self.level_image.get_rect()
-        self.level_rect.right = self.score_rect.right
-        self.level_rect.top = self.score_rect.bottom + 10
 
     def prep_ships(self):
         """Сообщает количество оставшихся кораблей."""
